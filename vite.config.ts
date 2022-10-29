d1d2c2decf54a9eec5b27471fd4332b05f8fd859
José Berríos <surphury@gmail.com>
2022-10-29 11:54:45 -0400

    feat: setup testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+
 import react from '@vitejs/plugin-react'
+
+import * as path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: '@', replacement: path.resolve(__dirname, 'src') },
+            {
+                find: './runtimeConfig',
+                replacement: './runtimeConfig.browser',
+            },
+        ],
+    },
 })
