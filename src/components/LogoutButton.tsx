36b83a093d090453786b71cd8e70d1cc26ff2700
José Berríos <surphury@gmail.com>
2022-10-30 14:24:28 -0400

    refactor: move file

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,28 +1,27 @@
-//@ts-nocheck
 import { useNavigate } from 'react-router-dom';
-import { UserAuth } from '../../context/AuthContext';
 
-function LogoutButton() {
-  const { logout } = UserAuth();
+import { Button } from '@/components';
+
+import { logOut } from '@/services';
+
+export function LogoutButton() {
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-      await logout();
+      await logOut();
       navigate('/signin');
     } catch (e) {
-      console.log(e.message);
+      console.log(e);
     }
   };
 
   return (
-    <button
+    <Button
       className="bg-blue-primary text-white px-4 py-2 rounded-lg hover:scale-105 transition duration-500 ease-in-out hover:bg-blue-secondary w-fit"
       onClick={handleLogout}
     >
       Disconnect
-    </button>
+    </Button>
   );
 }
-
-export default LogoutButton;
