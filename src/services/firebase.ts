e0a8a5dbabf889793880425abcd14e3791b68cfe
José Berríos <surphury@gmail.com>
2022-10-30 09:25:24 -0400

    fix: remove private keys from file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,13 +6,15 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCciQ7bgnphm9X0KMpSjTDBDN3GOegbCtA',
-  authDomain: 'hackaton-group3-fcd62.firebaseapp.com',
-  projectId: 'hackaton-group3-fcd62',
-  storageBucket: 'hackaton-group3-fcd62.appspot.com',
-  messagingSenderId: '781785767515',
-  appId: '1:781785767515:web:6fa74be5b4eb7977d7e97e',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
