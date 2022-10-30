b891a378315c8678f82784ca9fc3a73d3514750f
José Berríos <surphury@gmail.com>
2022-10-30 16:24:12 -0400

    fix: component has no types

--- src/view/Home/Categories/Categories.tsx
+++ src/view/Home/Categories/Categories.tsx
@@ -1,98 +1,94 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-export interface CategoriesInterface { }
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Pagination from '@mui/material/Pagination'
 
-import Pagination from '@mui/material/Pagination';
-import { Link } from 'react-router-dom';
-import { Search } from '../Search';
+import { Search } from '../Search'
+import { QueryJob } from '../../../models'
+import { getJobsByCategory } from '../../../services'
 
+export function Categories() {
+    const [jobs, setJobs] = useState<QueryJob[]>([])
+    const [page, setPage] = useState(1)
+    const [isFetching, setIsFetching] = useState(false)
 
+    const handleChange = (page: number) => {
+        setPage(page)
+    }
 
+    const query = new URLSearchParams(window.location.search)
 
+    const category = query.get('categoryID') as string
+    console.log(category)
 
-const Categories: React.FC<CategoriesInterface> = () => {
+    useEffect(() => {
+        setIsFetching(true)
 
-	const [jobs, setJobs] = useState([]);
-	const [page, setPage] = useState(1)
+        getJobsByCategory(category, 9, page)
+            .then((res) => setJobs(res.data))
+            .finally(() => {
+                setIsFetching(false)
+                window.scroll(0, 0)
+            })
+    }, [page])
 
-	const handleChange = (p) => {
-		setPage(p)
-		window.scroll(0, 0)
-	}
+    function truncate(str: string, n: number) {
+        return str?.length > n ? str.substr(0, n - 1) + ' ...' : str
+    }
 
-	let query = new URLSearchParams(window.location.search);
+    return (
+        <>
+            <h2 className="subt__cat">Empleos con la categoria {category}</h2>
 
-	let category = query.get('categoryID');
-	console.log(category)
+            <article className="container">
+                {jobs.map((job, i) => {
+                    return (
+                        <div key={i} className="container__card" id={job.id}>
+                            <h1>{job.attributes.title}</h1>
+                            <div className="container__description">
+                                <p className="container__description-title">
+                                    Descripcion:
+                                </p>
+                                {/*<p>{truncate(el.attributes.description, 150)}</p>*/}
+                                <p
+                                    dangerouslySetInnerHTML={{
+                                        __html: truncate(
+                                            job.attributes.description,
+                                            150
+                                        ),
+                                    }}
+                                ></p>
+                            </div>
 
-	const APIGet = `https://www.getonbrd.com/api/v0/categories/${category}/jobs?per_page=9&page=${page}`
+                            <div className="container__description">
+                                <a
+                                    href={job.links.public_url}
+                                    target="blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Visita esta oferta en GetOnBoard
+                                </a>
+                            </div>
+                        </div>
+                    )
+                })}
+            </article>
 
-	//console.log(APIGet)
+            <div className="pagination">
+                <p>Estás en la página {page}</p>
+                <Pagination
+                    count={10}
+                    variant="outlined"
+                    shape="rounded"
+                    disabled={isFetching}
+                    onChange={(e, page) => handleChange(page)}
+                />
+            </div>
 
-	useEffect(() => {
-		const endPoint = APIGet;
-		axios.get(endPoint)
-			.then(response => {
-				const apiData = response.data;
-				const apiAt = apiData.data
-				//console.log(apiAt)
-				setJobs(apiAt)
-			})
-	}, [page]);
+            <Search />
 
-	function truncate(str, n) {
-		return str?.length > n ? str.substr(0, n - 1) + " ..." : str;
-
-	}
-
-
-	return (
-		<>
-
-			<h2 className='subt__cat'>Empleos con la categoria {category}</h2>
-
-
-			<article className='container'>
-				{
-					jobs.map((el, i) => {
-						return (
-							<div key={i} className='container__card' id={el.id}>
-
-								<h1>{el.attributes.title}</h1>
-								<div className='container__description'>
-									<p className='container__description-title'>Descripcion:</p>
-									{/*<p>{truncate(el.attributes.description, 150)}</p>*/}
-									<p dangerouslySetInnerHTML={{ __html: truncate(el.attributes.description, 150) }}></p>
-								</div>
-
-
-
-
-
-								<div className='container__description'>
-									<a href={el.links.public_url} target='blank' rel='noopener noreferrer'>Visita esta oferta en GetOnBoard</a>
-								</div>
-							</div>
-						)
-					})
-				}
-
-			</article>
-
-			<div className='pagination'>
-				<p>Estas en la pagina {page}</p>
-				<Pagination count={10} variant="outlined" shape="rounded" onChange={(e) => handleChange(e.target.textContent)} />
-			</div>
-
-			<Search />
-
-			<Link to={`/`} className='card__cat'>
-				<h1>Volver al inicio</h1>
-			</Link>
-
-		</>
-
-	);
-};
-
-export default Categories;
+            <Link to="/" className="card__cat">
+                <h1>Volver al inicio</h1>
+            </Link>
+        </>
+    )
+}
