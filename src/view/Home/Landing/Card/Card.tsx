d090b8db5c20895364b7f87c82778242267be2c0
José Berríos <surphury@gmail.com>
2022-10-30 16:19:53 -0400

    export card as named

--- src/view/Home/Landing/Card/Card.tsx
+++ src/view/Home/Landing/Card/Card.tsx
@@ -1,51 +1,29 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-export interface CardInterface { }
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Card: React.FC<CardInterface> = () => {
+import { Category } from '@/models'
+import { getCategories } from '../../../../services'
 
-	const [category, setCategory] = useState([])
+export function Card() {
+    const [category, setCategory] = useState<Category[]>([])
 
-	const APIGet = `https://www.getonbrd.com/api/v0/categories/`
+    useEffect(() => {
+        getCategories().then((response) => setCategory(response.data))
+    }, [])
 
-	//console.log(APIGet)
-
-	useEffect(() => {
-		const endPoint = APIGet;
-		axios.get(endPoint)
-			.then(response => {
-				const apiData = response.data;
-				const apiAt = apiData.data
-				setCategory(apiAt)
-			})
-	}, [category]);
-
-
-	return (
-		<>
-
-			<h2 className='subt__cat container__card1'>Elige una categoria de empleo</h2>
-			<article className='container '>
-
-				{
-					category.map((el, i) => {
-						return (
-
-
-
-							<Link to={`/categories?categoryID=${el.id}`} key={i} className='card__cat' id={el.id}>
-
-								<h1>{el.attributes.name}</h1>
-
-							</Link>
-						)
-					})
-				}
-
-			</article>
-		</>
-	);
-};
-
-export default Card;
+    return (
+        <article className="container ">
+            {category.map((category) => {
+                return (
+                    <Link
+                        to={`/categories?categoryID=${category.id}`}
+                        className="card__cat"
+                        id={category.id}
+                    >
+                        <h1>{category.attributes.name}</h1>
+                    </Link>
+                )
+            })}
+        </article>
+    )
+}
