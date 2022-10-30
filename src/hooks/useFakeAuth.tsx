59b834783044621b8f06a0f0f2a3a1a2f5e4226a
José Berríos <surphury@gmail.com>
2022-10-30 14:24:52 -0400

    refactor: remove not used files

--- src/hooks/useFakeAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { FakeAuthContext } from '@/store/FakeAuthContext';
-
-const useFakeAuth = () => {
-  return useContext(FakeAuthContext);
-};
-
-export default useFakeAuth;
