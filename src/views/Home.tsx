734b0ba88f77369ba970f4ee2dcbfdcedfca9336
José Berríos <surphury@gmail.com>
2022-10-30 18:49:53 -0400

    style: format

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -22,16 +22,18 @@ export function Home() {
   );
 
   return (
-    <Suspense>
+    <>
       <h2 className="subtitle">
         Ofertas de Empleo disponibles en AgarraLaPala
       </h2>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </Suspense>
+      <Suspense fallback={''}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 /*
