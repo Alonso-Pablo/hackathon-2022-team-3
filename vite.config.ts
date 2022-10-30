6a6d9b343ba34470592ddf4ed77421121efd97b5
José Berríos <surphury@gmail.com>
2022-10-30 10:18:08 -0400

    Revert "Merge branch 'main' of github.com:Alonso-Pablo/hackathon-2022-team-3 into main"
    This reverts commit 693147eb2681782976daf94063bb3f6d87d15f19, reversing
    changes made to fbb3ea914c3f11b1abca4020f2165243bffe81c2.

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import * as path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    test: {
-        environment: 'jsdom',
-    },
     resolve: {
         alias: [
             { find: '@', replacement: path.resolve(__dirname, 'src') },
