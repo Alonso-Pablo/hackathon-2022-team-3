5b8134ee549f4ab3560a34a175c584c4d5f0178e
José Berríos <surphury@gmail.com>
2022-10-30 16:54:15 -0400

    refactor: remove not needed code

--- src/view/Home/Search/Results/Results.tsx
+++ src/view/Home/Search/Results/Results.tsx
@@ -1,81 +1,69 @@
-import React from 'react';
-export interface ResultsInterface { }
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Search from '../Search';
-import Pagination from '@mui/material/Pagination';
+import { useEffect, useState } from 'react'
 
+import Search from '../Search'
 
-const Results: React.FC<ResultsInterface> = () => {
+import { truncate } from '@/utilities'
+import { QueryJob } from '@/models'
+import { queryJobByName } from '@/services'
 
-	let query = new URLSearchParams(window.location.search);
+export function Results() {
+    let query = new URLSearchParams(window.location.search)
 
-	//console.log(query)
+    //console.log(query)
 
-	let keyword = query.get('name');
+    let keyword = query.get('name') as string
 
-	//console.log(keyword);
+    //console.log(keyword);
 
-	const [results, setResults] = useState([]);
+    const [results, setResults] = useState<QueryJob[]>([])
 
-	useEffect(() => {
-		const endPoint = `https://www.getonbrd.com/api/v0/search/jobs?query=${keyword}`
-		//console.log(endPoint)
-		axios.get(endPoint)
-			.then(response => {
-				const jobData = response.data;
-				const jobAt = jobData.data;
-				console.log(jobAt);
+    useEffect(() => {
+        queryJobByName(keyword, 9).then((res) => {
+            setResults(res.data)
+        })
+    }, [])
 
-				setResults(jobAt)
-			})
+    return (
+        <>
+            <h2 className="subt__cat">
+                <>Empleos encontrados para la busqueda: {results}</>
+            </h2>
 
-	}, [setResults]);
+            <article className="container">
+                {results.map((el, i) => {
+                    return (
+                        <div key={i} className="container__card">
+                            <h1>{el.attributes.title}</h1>
+                            <div className="container__description">
+                                <p className="container__description-title">
+                                    Descripcion:
+                                </p>
+                                {/*<p>{truncate(el.attributes.description, 150)}</p>*/}
+                                <p
+                                    dangerouslySetInnerHTML={{
+                                        __html: truncate(
+                                            el.attributes.description,
+                                            150
+                                        ),
+                                    }}
+                                ></p>
+                            </div>
 
-	function truncate(str, n) {
-		return str?.length > n ? str.substr(0, n - 1) + " ..." : str;
+                            <div className="container__description">
+                                <a
+                                    href={el.links.public_url}
+                                    target="blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Visita esta oferta en GetOnBoard
+                                </a>
+                            </div>
+                        </div>
+                    )
+                })}
+            </article>
 
-	}
-
-
-	return (
-		<>
-
-			<h2 className='subt__cat'>Empleos encontrados para la busqueda: {results}</h2>
-
-
-			<article className='container'>
-				{
-					results.map((el, i) => {
-						return (
-							<div key={i} className='container__card'>
-
-								<h1>{el.attributes.title}</h1>
-								<div className='container__description'>
-									<p className='container__description-title'>Descripcion:</p>
-									{/*<p>{truncate(el.attributes.description, 150)}</p>*/}
-									<p dangerouslySetInnerHTML={{ __html: truncate(el.attributes.description, 150) }}></p>
-								</div>
-
-
-
-
-
-								<div className='container__description'>
-									<a href={el.links.public_url} target='blank' rel='noopener noreferrer'>Visita esta oferta en GetOnBoard</a>
-								</div>
-							</div>
-						)
-					})
-				}
-
-			</article>
-
-
-
-			<Search />
-		</>
-	);
-};
-
-export default Results;
+            <Search />
+        </>
+    )
+}
