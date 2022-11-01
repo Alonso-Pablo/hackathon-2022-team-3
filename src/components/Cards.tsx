2f648e708c52b819d90b8fff9f8e31c98143b37f
José Berríos <surphury@gmail.com>
2022-10-31 20:12:44 -0400

    feat: remove empty filters

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,46 +1,113 @@
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { getCategories } from '@/services';
-import { Select } from '@/components';
-import { useSearchParams } from "react-router-dom";
+import { Select, Field } from '@/components';
+import { useSearchParams } from 'react-router-dom';
 import { SelectOptionsModel } from '@/models/selectOptionsModel';
 import { FilterModel } from '@/models/FilterModel';
-import { getCompanies, getModalities, getPerks, getSeniorities } from '@/services/selectOptionsService';
+import {
+  getCompanies,
+  getModalities,
+  getPerks,
+  getSeniorities,
+} from '@/services/selectOptionsService';
 
 export function Cards() {
+  const { register, watch } = useForm<{ location: string }>({
+    mode: 'onChange',
+  });
+
   const [selectOptions, setSelectOptions] = useState<SelectOptionsModel>({});
   const [filterParams, setFilterParams] = useState<FilterModel>({});
-  const [_, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const location = watch('location');
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {});
+    /* searchParams.delete('country'); */
+    return () => subscription.unsubscribe();
+  }, [watch]);
 
   const handleFilter = (param: FilterModel) => {
-    // const newParams = { ...filterParams, ...param };
+    const key = Object.keys(param)[0] as keyof typeof param;
+
+    if (param[key]?.length === 0) {
+      searchParams.delete(param[key] as string);
+    }
+
     const newParams = { ...param };
     setSearchParams(newParams);
     setFilterParams(newParams);
   };
 
   useEffect(() => {
-    const getInitialFilters = async () => {
-      const categoryResponse = await Promise.all([getCategories(), getSeniorities(), getModalities(), getCompanies(), getPerks()])
-      setSelectOptions({
-        category: categoryResponse[0].data,
-        seniority: categoryResponse[1].data,
-        modality: categoryResponse[2].data,
-        company: categoryResponse[3].data,
-        perks: categoryResponse[4].data,
-      });
-    }
-    getInitialFilters()
+    handleFilter({ country: location });
+  }, [location]);
+
+  const getInitialFilters = async () => {
+    const categoryResponse = await Promise.all([
+      getCategories(),
+      getSeniorities(),
+      getModalities(),
+      getCompanies(),
+      getPerks(),
+    ]);
+
+    setSelectOptions({
+      category: categoryResponse[0].data,
+      seniority: categoryResponse[1].data,
+      modality: categoryResponse[2].data,
+      company: categoryResponse[3].data,
+      perks: categoryResponse[4].data,
+    });
+  };
+
+  useEffect(() => {
+    getInitialFilters();
   }, []);
 
   return (
-    <>
-      {selectOptions?.category && (<Select text="Category" options={selectOptions?.category} onChange={(selected) => handleFilter({ category: selected })} />)}
-      {selectOptions?.modality && (<Select by='locale_key' text="Modality" options={selectOptions?.modality} onChange={(selected) => handleFilter({ modality : selected })} />)}
-      {selectOptions?.company && (<Select text="Company" options={selectOptions?.company} onChange={(selected) => handleFilter({ company: selected })} />)}
-      {selectOptions?.seniority && (<Select by='locale_key' text="Seniority" options={selectOptions?.seniority} onChange={(selected) => handleFilter({ seniority: selected })} />)}
-      {selectOptions?.perks && (<Select text="Perk" options={selectOptions?.perks} onChange={(selected) => handleFilter({ perks: selected })} />)}
-    </>
+    <div className="flex flex-wrap gap-4">
+      {selectOptions?.category && (
+        <Select
+          text="Categoría"
+          options={selectOptions?.category}
+          onChange={(selected) => handleFilter({ category: selected })}
+        />
+      )}
+      {selectOptions?.modality && (
+        <Select
+          by="locale_key"
+          text="Modalidad"
+          options={selectOptions?.modality}
+          onChange={(selected) => handleFilter({ modality: selected })}
+        />
+      )}
+      {selectOptions?.company && (
+        <Select
+          text="Compañía"
+          options={selectOptions?.company}
+          onChange={(selected) => handleFilter({ company: selected })}
+        />
+      )}
+      {selectOptions?.seniority && (
+        <Select
+          by="locale_key"
+          text="Seniority"
+          options={selectOptions?.seniority}
+          onChange={(selected) => handleFilter({ seniority: selected })}
+        />
+      )}
+      {selectOptions?.perks && (
+        <Select
+          text="Beneficios"
+          options={selectOptions?.perks}
+          onChange={(selected) => handleFilter({ perks: selected })}
+        />
+      )}
+      <Field text="País" register={register('location', { required: true })} />
+    </div>
   );
-
 }
