4038d2c6605c771addb72c3d1063059c85ab9739
José Berríos <surphury@gmail.com>
2022-10-31 20:13:35 -0400

    feat: add MUI Theme

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,22 +6,40 @@ import SignUp from '@/components/Login/SignUp';
 import DashboardPrivateRoute from './privateRouter/DashboardPrivateRoute';
 import PrivateRouter from './privateRouter/PrivateRouter';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+/* if (
+  )
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+} */
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+      <ThemeProvider theme={darkTheme}>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
 
-        <Route
-          path="*"
-          element={
-            <PrivateRouter>
-              <DashboardPrivateRoute />
-            </PrivateRouter>
-          }
-        />
-      </Routes>
+          <Route
+            path="*"
+            element={
+              <PrivateRouter>
+                <DashboardPrivateRoute />
+              </PrivateRouter>
+            }
+          />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
